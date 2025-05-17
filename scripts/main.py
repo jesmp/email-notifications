89b658d3b6ffed45315de19051ddadbc5c97e90c
jesmp <jmp.0711@yahoo.com>
2025-05-17 14:11:28 -0500

    updated code

--- scripts/main.py
+++ scripts/main.py
@@ -1,27 +1,31 @@
 import smtplib
 import os
+import weather_app_info
+
 from email.message import EmailMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# --- CONFIGURATION ---
+# Configurations
 gmail_address = os.getenv('GMAIL_EMAIL')
 app_password = os.getenv('APP_PASSWORD')
 to_number = os.getenv('ATT_PHONE_NUMBER')
 to_email = os.getenv('YAHOO_EMAIL')
 message_text = 'Hello from Python via Gmail!'
 
-# --- EMAIL SETUP ---
-msg = EmailMessage()
-msg.set_content(message_text)
-msg['From'] = gmail_address
-msg['To'] = to_email
-msg['Subject'] = 'Personal Alert Assistant(PAA)'
+weather = weather_app_info.WeatherAppInfo().organize_weather_data()
 
-# --- SEND EMAIL ---
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(gmail_address, app_password)
-    smtp.send_message(msg)
+# # Email Setup
+# msg = EmailMessage()
+# msg.set_content(message_text)
+# msg['From'] = gmail_address
+# msg['To'] = to_email
+# msg['Subject'] = 'Personal Alert Assistant(PAA)'
+#
+# # Send Email
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+#     smtp.login(gmail_address, app_password)
+#     smtp.send_message(msg)
 
-print("Email sent!")
+print(weather)
--- scripts/weather_app_info.py
+++ scripts/weather_app_info.py
@@ -1,4 +1,6 @@
 import os
+import requests
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,9 +8,26 @@ load_dotenv()
 class WeatherAppInfo:
     def __init__(self):
         self.weather_key = os.getenv('OPEN_WEATHER_API_KEY')
+        self.cw_url = os.getenv('CURRENT_WEATHER_URL')
+        self.hw_url = os.getenv('HOURLY_WEATHER_URL')
+        self.latitude = os.getenv('HOME_LAT')
+        self.longitude = os.getenv('HOME_LON')
 
     def get_weather_api_data(self):
-        pass
+        params = {
+            'lat':self.latitude,
+            'lon':self.longitude,
+            'appid':self.weather_key,
+            'units':'imperial'
+        }
+        response = requests.get(self.cw_url,params=params)
+        response = response.text
+        return response
 
     def organize_weather_data(self):
-        pass
+        data = self.get_weather_api_data()
+
+        return data
+
+
+
