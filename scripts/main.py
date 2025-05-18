a044b28a496676d78a446b439d7391dfbe8934b3
jesmp <jmp.0711@yahoo.com>
2025-05-18 12:47:55 -0500

    Finished Weather Data message

--- scripts/main.py
+++ scripts/main.py
@@ -12,20 +12,17 @@ gmail_address = os.getenv('GMAIL_EMAIL')
 app_password = os.getenv('APP_PASSWORD')
 to_number = os.getenv('ATT_PHONE_NUMBER')
 to_email = os.getenv('YAHOO_EMAIL')
-message_text = 'Hello from Python via Gmail!'
-
 weather = weather_app_info.WeatherAppInfo().organize_weather_data()
+message_text = weather
 
-# # Email Setup
-# msg = EmailMessage()
-# msg.set_content(message_text)
-# msg['From'] = gmail_address
-# msg['To'] = to_email
-# msg['Subject'] = 'Personal Alert Assistant(PAA)'
-#
-# # Send Email
-# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#     smtp.login(gmail_address, app_password)
-#     smtp.send_message(msg)
+# Email Setup
+msg = EmailMessage()
+msg.set_content(message_text)
+msg['From'] = gmail_address
+msg['To'] = to_email
+msg['Subject'] = 'Personal Alert Assistant(PAA)'
 
-print(weather)
+# Send Email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(gmail_address, app_password)
+    smtp.send_message(msg)
--- scripts/weather_app_info.py
+++ scripts/weather_app_info.py
@@ -16,13 +16,14 @@ class WeatherAppInfo:
         self.hw_url = os.getenv('HOURLY_WEATHER_URL')
         self.latitude = os.getenv('HOME_LAT')
         self.longitude = os.getenv('HOME_LON')
+        self.weather_alerts = []
 
     def get_current_weather_api_data(self):
         params = {
             'lat':self.latitude,
             'lon':self.longitude,
-            'appid':self.weather_key,
-            'units':'imperial'
+            'units': 'imperial',
+            'appid':self.weather_key
         }
         response = requests.get(self.cw_url,params=params)
         return response.json()
@@ -31,6 +32,8 @@ class WeatherAppInfo:
         params = {
             'lat':self.latitude,
             'lon':self.longitude,
+            'units':'imperial',
+            'cnt':4,
             'appid':self.weather_key
         }
         response = requests.get(self.hw_url,params=params)
@@ -38,7 +41,11 @@ class WeatherAppInfo:
 
     def organize_weather_data(self):
         cw_data = self.get_current_weather_api_data()
-        hw_data =
+        hw_data = self.get_hourly_weather_api_data()
+
+        weather_alert_message = self.check_weather(hw_data)
+        full_weather_alert_string = "\n".join(weather_alert_message)
+
         sunrise_time = self.convert_unix_time(cw_data['sys']['sunrise'])
         sunset_time = self.convert_unix_time(cw_data['sys']['sunset'])
 
@@ -49,7 +56,7 @@ class WeatherAppInfo:
         sun_time = f"Sunrise is at {sunrise_time} and Sunset is at {sunset_time}\n"
 
         full_message = (current_weather + current_temperature + current_feels_like +
-                        current_humidity + sun_time)
+                        current_humidity + sun_time + "\n\n" + full_weather_alert_string)
 
         return full_message
 
@@ -59,3 +66,26 @@ class WeatherAppInfo:
         cst_time = utc_dt.astimezone(cst)
         formatted_time = cst_time.strftime("%H:%M:%S")
         return formatted_time
+
+    def check_weather(self,hours):
+        for hour in hours['list']:
+            timestamp = self.convert_unix_time(hour['dt'])
+            weather_id = int(hour['weather'][0]['id'])
+            if 200 <= weather_id < 600:
+                msg_alert = "Pack an umbrella.☂️"
+            elif 600 <= weather_id < 700:
+                msg_alert = "Stay warm and pack a jacket.🧥"
+            elif 700 <= weather_id < 800:
+                msg_alert = "Pack for special conditions."
+            else:
+                msg_alert = "Enjoy the day! Stay Hydrated!🌊"
+
+            message = (f"At {timestamp}, "
+                       f"\nForecast: {hour['weather'][0]['main']}:{hour['weather'][0]['description']}, "
+                       f"\nTemperature:{hour['main']['temp']}\u00b0F"
+                       f"\nFeels Like:{hour['main']['feels_like']}\u00b0F"
+                       f"\nHumidity:{hour['main']['humidity']}"
+                       f"\nMessage Alert:{msg_alert}\n")
+            self.weather_alerts.append(message)
+
+        return self.weather_alerts
