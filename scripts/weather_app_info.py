21cb8574fb10453237e0a9cd816ce77b2f5f3d19
jesmp <jmp.0711@yahoo.com>
2025-05-18 11:28:05 -0500

    Updated data

--- scripts/weather_app_info.py
+++ scripts/weather_app_info.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+
 import os
 import requests
+import pytz
 
 from dotenv import load_dotenv
 
@@ -13,7 +17,7 @@ class WeatherAppInfo:
         self.latitude = os.getenv('HOME_LAT')
         self.longitude = os.getenv('HOME_LON')
 
-    def get_weather_api_data(self):
+    def get_current_weather_api_data(self):
         params = {
             'lat':self.latitude,
             'lon':self.longitude,
@@ -21,13 +25,37 @@ class WeatherAppInfo:
             'units':'imperial'
         }
         response = requests.get(self.cw_url,params=params)
-        response = response.text
-        return response
+        return response.json()
+
+    def get_hourly_weather_api_data(self):
+        params = {
+            'lat':self.latitude,
+            'lon':self.longitude,
+            'appid':self.weather_key
+        }
+        response = requests.get(self.hw_url,params=params)
+        return response.json()
 
     def organize_weather_data(self):
-        data = self.get_weather_api_data()
+        cw_data = self.get_current_weather_api_data()
+        hw_data =
+        sunrise_time = self.convert_unix_time(cw_data['sys']['sunrise'])
+        sunset_time = self.convert_unix_time(cw_data['sys']['sunset'])
 
-        # return data
+        current_weather = f"Today's weather is {cw_data['weather'][0]['description']}\n"
+        current_temperature = f"Temperature is {cw_data['main']['temp']}\u00b0F\n"
+        current_feels_like = f"Feels like {cw_data['main']['feels_like']}\u00b0F\n"
+        current_humidity = f"Humidity is at {cw_data['main']['humidity']}\n"
+        sun_time = f"Sunrise is at {sunrise_time} and Sunset is at {sunset_time}\n"
 
+        full_message = (current_weather + current_temperature + current_feels_like +
+                        current_humidity + sun_time)
 
+        return full_message
 
+    def convert_unix_time(self,time):
+        utc_dt = datetime.fromtimestamp(time,tz=timezone.utc)
+        cst = pytz.timezone("America/Chicago")
+        cst_time = utc_dt.astimezone(cst)
+        formatted_time = cst_time.strftime("%H:%M:%S")
+        return formatted_time
